4d78c0cc68d12df877ef75c4e24c028ccf668ed8
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-08-02 21:26:16 +0300

    First API

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = require('./routes/index');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
