38d21de9fd6154d696217075295e08073c25b5ef
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-08-02 21:00:47 +0000

    Temp

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
+const FilesController = require('../controllers/FilesController')
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
