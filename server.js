243dd35086d864fb7953a9d26b74dd389b92d5ce
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-08-02 22:34:44 +0000

    First file

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use('/', routes); // Make sure routes is a valid router
+app.use(express.json());
+app.use('/', routes);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port http://0.0.0.0:${port}`);
 });
