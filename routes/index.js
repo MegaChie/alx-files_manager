370725b8a4ee14dcbbf949c6ff608704ccb07eff
abdelrahman21 <boodyhussein12@gmail.com>
2024-08-06 02:03:45 +0300

    uncommented getDisconnect endpoint in index.js

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/files', FilesController.getIndex);
 router.get('/files/:id/data', FilesController.getFile);
 router.get('/connect',AuthController.getConnect);
 //router.get('/users/me',UserController.getMe);
-//router.post('/files', FilesController.getDisconnect);
+router.post('/files', FilesController.getDisconnect);
 
 router.post('/users', UserController.postNew);
 router.put('/files/:id/publish', FilesController.putPublish);
