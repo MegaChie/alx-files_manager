287def8199732effcddc0ebebd11196e6cd463db
abdelrahman21 <boodyhussein12@gmail.com>
2024-08-05 16:45:29 +0300

    Auth control, index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const FilesController = require('../controllers/FilesController');
 const UserController = require('../controllers/UsersController');
-
+const AuthController = require('../controllers/AuthController');
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
@@ -10,7 +10,11 @@ router.get('/stats', AppController.getStats);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 router.get('/files/:id/data', FilesController.getFile);
-router.post('/files', FilesController.postUpload);
+router.get('/connect',AuthController.getConnect);
+router.get('/disconnect',AuthController.getConnect);
+router.get('/users/me',UserController.getMe);
+router.post('/files', FilesController.getDisconnect);
+
 router.post('/users', UserController.postNew);
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
